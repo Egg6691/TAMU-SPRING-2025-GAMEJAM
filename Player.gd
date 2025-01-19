e21e8e2a6a55536a505b59d0888a9e05337f6180
Brandon (Egg) <egg@tamu.edu>
2025-01-18 20:40:23 -0600

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -1,34 +1,22 @@
 extends Character
 
-export (int) var speed = 64*3 # Tiles per second
 
-var velocity = Vector2()
-var input_buffer = [Vector2.ZERO]
-var input_buffer_readout = Vector2()
+export var speed = 64 * 3 # tiles per second
+var velocity = Vector2.ZERO
 
-func _physics_process(_delta): # the code appends the input to a buffer, which only allows one direction of movement at a time.
-	if Input.is_action_just_pressed("right"):
-		input_buffer.append(Vector2.RIGHT)
-	elif Input.is_action_just_pressed("left"):
-		input_buffer.append(Vector2.LEFT)
-	elif Input.is_action_just_pressed("up"):
-		input_buffer.append(Vector2.UP)
-	elif Input.is_action_just_pressed("down"):
-		input_buffer.append(Vector2.DOWN)
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	# Make sure diagonal movement isn't faster
+	velocity = velocity.normalized() * speed
 
-	if Input.is_action_just_released("right"):
-		input_buffer.erase(Vector2.RIGHT)
-	elif Input.is_action_just_released("left"):
-		input_buffer.erase(Vector2.LEFT)
-	elif Input.is_action_just_released("up"):
-		input_buffer.erase(Vector2.UP)
-	elif Input.is_action_just_released("down"):
-		input_buffer.erase(Vector2.DOWN)
-
-	input_buffer_readout = input_buffer[-1]
-
-#	print(input_buffer)
-#	print(input_buffer_readout)
-
-	velocity = input_buffer_readout * speed
+func _physics_process(delta):
+	get_input()
 	velocity = move_and_slide(velocity)
